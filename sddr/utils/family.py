fe8bf466db2efa1ff25f6bf1657813bcf816021a
felixGer <39033536+felixGer@users.noreply.github.com>
2022-01-05 16:50:45 +0100

    Update family.py

--- sddr/utils/family.py
+++ sddr/utils/family.py
@@ -127,7 +127,7 @@ class Family():
              ####### to do: loc, scale -> f(total count) , p(probs)
 
             #pred_trafo["probs"] = (pred["total_count"].exp()*pred["probs"].exp()) / (1 + pred["total_count"].exp()*pred["probs"].exp())
-            pred_trafo["probs"] = (torch.nan_to_num(pred["probs"].exp(), nan = 0, posinf=10e6, neginf=-10e6)*(1-add_const))/(torch.nan_to_num(pred["probs"].exp(), nan = 0, posinf=10e6, neginf=-10e6)+ torch.nan_to_num(pred["total_count"].exp(),  nan = 0, posinf=10e6, neginf=-10e6) +0.1*add_const) 
+            pred_trafo["probs"] = (torch.clamp_max_(pred["probs"].exp(), 10e6)*(1-add_const))/(torch.clamp_max_(pred["probs"].exp(), 10e6)+ torch.clamp_max_(pred["total_count"].exp(), 10e6) +0.1*add_const) 
             #pred_trafo["total_count"] = 1/ (add_const + pred["probs"].exp())
             pred_trafo["total_count"] = pred["total_count"].exp()
 
