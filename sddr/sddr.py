ff57c8f1611678fdc364de913c0f30d559d60575
felixGer <39033536+felixGer@users.noreply.github.com>
2022-01-20 17:02:04 +0100

    Update sddr.py

--- sddr/sddr.py
+++ sddr/sddr.py
@@ -345,11 +345,11 @@ class Sddr(object):
         #save predictions
         with torch.no_grad():
             #self.val_individual_NLL  = torch.index_select(self.net.get_log_loss(target).to(self.device), 0, torch.tensor(test_indices).to(self.device)).to(self.device)
-            self.val_individual_NLL  = torch.index_select(self.net.get_log_loss(target).to(self.device), 0, torch.tensor(test_indices).numpy())
+            self.val_individual_NLL  = torch.index_select(self.net.get_log_loss(target), 0, torch.tensor(test_indices)).numpy()
             self.val_preds = dict()
             for param in datadict.keys():
                 #self.val_preds[param] = torch.index_select(vars(self.net(datadict,training=False))[param], 0, torch.tensor(test_indices).to(self.device))
-                self.val_preds[param] = torch.index_select(vars(self.net(datadict,training=False))[param], 0, torch.tensor(test_indices).numpy())
+                self.val_preds[param] = torch.index_select(vars(self.net(datadict,training=False))[param], 0, torch.tensor(test_indices)).numpy()
                 
             
         if plot:
